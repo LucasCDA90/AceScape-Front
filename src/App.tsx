32f55cdd33104980007aa5fd196200eb5915a58e
LucasCDA90 <lucas.brg7@gmail.com>
2024-07-23 08:54:54 +0200

    Page d'acceuil terminée

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import './Insfrastructure/Style/index.css'
+import '/Users/33669/Projet-Personnel/AceScape-Front/src/Insfrastructure/Style/index.css';
 import logo from './Application/images/logo-ace-scape.png'
 import { AuthLoginForm } from './Application/Components/AuthLoginForm'
 
 export const App = () => {
   return (
-    <div className='b-vert-fonce text-white'>
-      <img src={logo} alt=""/>
-      <span>"Join the AceScape adventure, where every card dealt leads to an epic journey through the realms of chance and strategy!"</span>
-      <span>"Life is like a deck of cards; it's not about the hand you're dealt, but how you play it."</span>
-      <AuthLoginForm/>
-
-      
+    <div className='b-vert-fonce text-white h-screen flex'>
+      <div className='w-1/2 flex items-center justify-center'>
+        <img src={logo} alt=""/>
+      </div>
+      <div className='w-1/2 flex flex-col justify-center items-start p-8'>
+        <div className='mb-8'>
+          <span className='block mb-4'>"Join the AceScape adventure, where every card dealt leads to an epic journey through the realms of chance and strategy!"</span>
+          <span>"Life is like a deck of cards; it's not about the hand you're dealt, but how you play it."</span>
+        </div>
+        <AuthLoginForm/>
+      </div>
     </div>
   )
 }
--- src/Application/Components/AuthLoginForm.tsx
+++ src/Application/Components/AuthLoginForm.tsx
@@ -1,15 +1,15 @@
 export const AuthLoginForm = () => {
     return (
         <form action="" className='flex flex-col w-64 text-white'>
-            <div className='flex flex-col'>
-                <label htmlFor="email" className="text-white">Email adress</label>
+            <div className='flex flex-col mb-4'>
+                <label htmlFor="email" className="text-white">Email address</label>
                 <input type="text" id="email" className="text-black"/>
             </div>
-            <div className='flex flex-col'>
+            <div className='flex flex-col mb-4'>
                 <label htmlFor="password">Password</label>
                 <input type="text" id="password" className="text-black"/>
             </div>
-            <a href="">Forgot password ?</a>
+            <a href="" className="mb-4">Forgot password ?</a>
             <div className="flex gap-4">
                 <button className="b-vert-clair2x p-4 rounded-md">Create account</button>
                 <button className="b-vert-clair2x p-4 rounded-md">Sign in</button>
