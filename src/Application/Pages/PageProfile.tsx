be7d7936265d638d9cdb5e7f8406479c5c458666
LucasCDA90 <lucas.brg7@gmail.com>
2024-07-29 16:12:33 +0200

    Commit intermediaire

--- src/Application/Pages/PageProfile.tsx
+++ src/Application/Pages/PageProfile.tsx
@@ -15,7 +15,8 @@ export const PageProfile = () => {
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const response = await axios.get('/user/66a7991c7978d393e05c2d30');
+        const response = await axios.get('/user'); //marche pas
+        //const response = await axios.get('http://localhost:3002/user/'); //marche pas non plus
         setUser(response.data);
       } catch (error) {
         console.error('Erreur lors de la récupération des informations utilisateur', error);
@@ -29,33 +30,36 @@ export const PageProfile = () => {
     <div className="flex flex-col items-center justify-center min-h-screen bg-green-900 p-6">
       <div className="bg-white text-gray-800 p-8 rounded-lg shadow-lg w-full max-w-4xl">
         <h2 className="text-3xl font-bold mb-6 text-center">Informations du Profil</h2>
-        {user ? (
-          <div className="space-y-6">
-            <div className="flex items-center">
-              <span className="font-semibold w-1/3">Prénom:</span>
-              <span>{user.firstName}</span>
-            </div>
-            <div className="flex items-center">
-              <span className="font-semibold w-1/3">Nom:</span>
-              <span>{user.lastName}</span>
-            </div>
-            <div className="flex items-center">
-              <span className="font-semibold w-1/3">Nom d'utilisateur:</span>
-              <span>{user.username}</span>
-            </div>
-            <div className="flex items-center">
-              <span className="font-semibold w-1/3">Email:</span>
-              <span>{user.email}</span>
-            </div>
-            <div className="mt-6 text-center">
-              <Link to="/profile/edit" className="inline-block bg-green-600 text-white py-2 px-4 rounded-md shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500">
-                Modifier le Profil
-              </Link>
-            </div>
-          </div>
-        ) : (
-          <div className="text-center text-gray-600">Chargement des informations du profil...</div>
-        )}
+        <div className="space-y-6">
+          {user && (
+            <>
+              <div className="flex items-center">
+                <span className="font-semibold w-1/3">Prénom:</span>
+                <span>{user.firstName}</span>
+              </div>
+              <div className="flex items-center">
+                <span className="font-semibold w-1/3">Nom:</span>
+                <span>{user.lastName}</span>
+              </div>
+              <div className="flex items-center">
+                <span className="font-semibold w-1/3">Nom d'utilisateur:</span>
+                <span>{user.username}</span>
+              </div>
+              <div className="flex items-center">
+                <span className="font-semibold w-1/3">Email:</span>
+                <span>{user.email}</span>
+              </div>
+              <div className="mt-6 text-center">
+                <Link to="/profile/edit" className="inline-block bg-green-600 text-white py-2 px-4 rounded-md shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500">
+                  Modifier le profil et/ou le mot de passe
+                </Link>
+              </div>
+            </>
+          )}
+          {!user && (
+            <div className="text-center text-gray-600">Chargement des informations du profil...</div>
+          )}
+        </div>
       </div>
     </div>
   );
