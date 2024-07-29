97a9963ab94ccedd3ee3edbfd98f600daf0ef671
LucasCDA90 <lucas.brg7@gmail.com>
2024-07-29 08:56:44 +0200

    Fix buggs

--- src/Infrastructure/Http.ts
+++ src/Infrastructure/Http.ts
@@ -4,7 +4,7 @@ export const http = axios.create({
     baseURL: "http://localhost:3002/"
 })
 
-const url = 'user/66a394ed1e1ab3b832f7657a';
+const url = 'user/66a642cc01ded19ee2782fc0';
 
 const data = {
   username: "John Doe"
@@ -18,15 +18,7 @@ const data = {
 };
 const config = {
   headers: {
-    'Authorization': "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmEzOTRlZDFlMWFiM2I4MzJmNzY1N2EiLCJpYXQiOjE3MjE5OTY1MjksImV4cCI6MTcyMjAwMzcyOX0.zrU9q0vIwIrAZTDtB65foeqt4I5X7AUYbK3QGeCLu80",
+    'Authorization': "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmE2NDJjYzAxZGVkMTllZTI3ODJmYzAiLCJpYXQiOjE3MjIxNzIxMTksImV4cCI6MTcyMjE3OTMxOX0.8xjSiYlHHKpDJ2RlnsjGPnNdGvxsYtnX-GpsabsHyOA",
     'Content-Type': 'application/json'
   }
 };
-
-http.put(url, data, config)
-  .then(response => {
-    console.log('Utilisateur mis à jour:', response.data);
-  })
-  .catch(error => {
-    console.error('Erreur lors de la mise à jour:', error);
-  });
